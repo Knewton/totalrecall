ed954c7c5e4a3b6ba9be7cfaf0f0f079d7c3f7b7
Michael Louis Thaler <michael.louis.thaler@gmail.com>
2011-12-09 13:41:51 -0500

    Add enter-game

--- memory.js
+++ memory.js
@@ -8,11 +8,47 @@ var express = require('express'),
     games = {}
 ;
 
+/**
+ * Returns a two-dimensional array of cards to start off a game.
+ */
+function generateCards(pairs) {
+
+}
+
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
 app.listen(3000);
 
 io.sockets.on('connection', function (socket) {
-    socket.emit('test', {hello: 'world'});
+    socket.on('enter-game', function (data) {
+        data.name = 'game_' + data.name;
+
+        if (!games.hasOwnProperty(data.name)) {
+            // Setup game if it doesn't exist
+            games[data.name] = {
+                exposed: {
+                    name: data.name,
+                    started: Math.round((new Date()).valueOf() / 1000),
+                    players: [data.player]
+                },
+                hidden: {
+                    cards: generateCards(8)
+                }
+            };
+        } else {
+            // Join game if it does
+            games[data.name].exposed.players.push(data.player);
+        }
+
+        // Set player stats
+        socket.set('name', data.player);
+        socket.set('score', 0);
+
+        // Join broadcasts for the game room
+        socket.join(data.name);
+
+        // Return data
+        socket.emit('game-info', games[data.name].exposed);
+    });
 });
