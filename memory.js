c7a44a512ac3e8063f7deb7b35ef9d1c58f3c013
Michael Louis Thaler <michael.louis.thaler@gmail.com>
2011-12-13 12:23:36 -0500

    Add announcement improvements

--- memory.js
+++ memory.js
@@ -50,6 +50,19 @@ function getCard(game, coords) {
     return games[game].hidden.cards[coords.x][coords.y];
 }
 
+function sendGameInfo(socket, game, broadcast) {
+    broadcast = broadcast || false;
+
+    var exposed = games[game].exposed;
+    exposed.name = exposed.name.replace(/^game_/, '');
+
+    if (broadcast) {
+        socket.broadcast.to(game).emit('game-info', exposed);
+    } else {
+        socket.emit('game-info', exposed);
+    }
+}
+
 function playerIsVictorious(stats) {
     return stats.flipped.length === (games[stats.game].hidden.cards.length / 2);
 }
@@ -63,6 +76,11 @@ io.sockets.on('connection', function (socket) {
 
     socket.on('disconnect', function () {
         socket.get('stats', function (err, stats) {
+            if (stats === null) {
+                // User has not joined a game
+                return;
+            }
+
             socket.leave(stats.game);
             socket.broadcast.to(stats.game).emit('announcement', stats.name + " left the game.");
 
@@ -94,12 +112,16 @@ io.sockets.on('connection', function (socket) {
                     card_count: 24
                 }
             };
-        }
+        } else {
 
-        // Player name is taken. Just stop.
-        if (data.player in games[data.name].exposed.players) {
-            socket.emit('error', "Player name " + player + " is taken.");
-            return;
+            // Player name is taken. Just stop.
+            if (data.player in games[data.name].exposed.players) {
+                socket.emit('error', "Player name " + player + " is taken.");
+                return;
+            }
+
+            socket.broadcast.to(data.name).emit('announcement', data.player + " joined the game!");
+            sendGameInfo(socket, data.name, true);
         }
 
         games[data.name].exposed.players[data.player] = 0;
@@ -116,9 +138,7 @@ io.sockets.on('connection', function (socket) {
         socket.join(data.name);
 
         // Return data
-        var exposed = games[data.name].exposed;
-        exposed.name = exposed.name.replace(/^game_/, '');
-        socket.emit('game-info', exposed);
+        sendGameInfo(socket, data.name, false);
     });
 
     // Player flips a card.
@@ -155,6 +175,7 @@ io.sockets.on('connection', function (socket) {
                         games[stats.game].exposed.players[stats.name]++;
 
                         socket.broadcast.to(stats.game).emit('announcement', stats.name + " got a point!");
+                        sendGameInfo(socket, stats.game, true);
 
                         // Evil code: pick a random other player and a random flipped pair
                         var candidates = [],
@@ -172,7 +193,11 @@ io.sockets.on('connection', function (socket) {
                             unlucky_sod = candidates[Math.ceil(Math.random() * candidates.length - 1)];
                             unlucky_sod_socket = io.sockets.sockets[unlucky_sod];
                             unlucky_sod_socket.get('stats', function (err, unlucky_stats) {
-                                if (unlucky_stats.flipped === null) {
+                                /* Checking unlucky_stats for null is a workaround for a bug: if a user disconnects,
+                                   they aren't removed from io.sockets.sockets. To fix it properly I need to somehow
+                                   exclude them from candidates
+                                */
+                                if (unlucky_stats === null || unlucky_stats.flipped === null) {
                                     return;
                                 }
 
@@ -198,7 +223,8 @@ io.sockets.on('connection', function (socket) {
                         socket.emit('card-flipped', {
                             flipover: [
                                 [data.x, data.y, card_identity]
-                            ]
+                            ],
+                            add_point: true
                         });
 
                         // Check for player victory and let players know if it happened
@@ -232,9 +258,12 @@ io.sockets.on('connection', function (socket) {
                 };
 
                 socket.set('stats', stats, function () {
-                    socket.emit('card-flipped', {flipover: [
-                        [data.x, data.y, card_identity]
-                    ]});
+                    socket.emit('card-flipped', {
+                        flipover: [
+                            [data.x, data.y, card_identity]
+                        ],
+                        add_point: false
+                    });
                 });
             }
         });
